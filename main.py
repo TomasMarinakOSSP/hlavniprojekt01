15a62408c878def043786f6642b2ffc41901ee91
TomasMarinakOSSP <tomas.marinak@student.ossp.cz>
2025-02-17 14:24:19 +0100

    vytvoření index.html a login.html předělání base.html a main.py přidání obrázků

--- main.py
+++ main.py
@@ -9,5 +9,19 @@ def about():
     return render_template('about.html')
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username in users and users[username] == password:
+            return redirect(url_for("index"))
+        else:
+            error = "Nesprávné jméno nebo heslo."
+
+    return render_template("login.html", error=error)
+
 if __name__ == '__main__':
     app.run(debug=True)
