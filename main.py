9d63e9ed35fdcf53023b9e912d4311671de893d2
TomasMarinakOSSP <tomas.marinak@student.ossp.cz>
2025-02-17 14:26:49 +0100

    v main jsem zmenil home za index.html

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
